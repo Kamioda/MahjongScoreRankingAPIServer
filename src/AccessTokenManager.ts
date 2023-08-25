6c130b7464a0e995f1bf1571f75e268031f7684e
Kamioda <aimegu2014@gmail.com>
2023-08-25 15:38:02 +0900

    アクセストークンの管理クラスを追加

---
+++ src/AccessTokenManager.ts
@@ -0,0 +1,28 @@
+import { generate as createRandomString } from 'randomstring';
+
+interface AccessTokenInfo {
+    [name: string]: string;
+}
+
+export default class AccessTokenManager {
+    #AccessTokens: AccessTokenInfo;
+    #TokenLen: number;
+    constructor(AccessTokenLength: number = 256) {
+        this.#AccessTokens = {};
+        this.#TokenLen = AccessTokenLength;
+    }
+    create(TargetAccountID: string): string {
+        const AccessToken = createRandomString({ length: this.#TokenLen, charset: 'alphanumeric' });
+        if (Object.keys(this.#AccessTokens).includes(AccessToken)) return this.create(TargetAccountID);
+        this.#AccessTokens[AccessToken] = TargetAccountID;
+        return AccessToken;
+    }
+    getId(Token: string): string {
+        return this.#AccessTokens[Token];
+    }
+    invalidation(Token: string): boolean {
+        if (!Object.keys(this.#AccessTokens).includes(Token)) return false;
+        delete this.#AccessTokens[Token];
+        return!Object.keys(this.#AccessTokens).includes(Token);
+    }
+}
